539349bbd4fe7a6f536596d8db2feeb29c6abe7c
Maxwell <anoliefomaxwell2@gmail.com>
2023-02-19 13:18:30 -0800

    tokenize

--- tokenize.c
+++ tokenize.c
@@ -2,6 +2,8 @@
 /**
  * tokenize - this function separate the string using a designed delimiter
  * @data: a pointer to the program's data
+ *
+ *
  * Return: an array of the different parts of the string
  */
 void tokenize(data_of_program *data)
